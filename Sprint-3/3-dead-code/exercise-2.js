d660772debbeb64b05171c36db454841b4f73d24
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-14 10:29:15 +0000

    Revert "removed dead code"
    This reverts commit de2ab2ffd017efd88ec812be72b834da8ec6e88e.

--- Sprint-3/3-dead-code/exercise-2.js
+++ Sprint-3/3-dead-code/exercise-2.js
@@ -2,7 +2,12 @@
 // The countAndCapitalisePets function should continue to work for any reasonable input it's given, and you shouldn't modify the pets variable.
 
 const pets = ["parrot", "hamster", "horse", "dog", "hamster", "cat", "hamster"];
+const capitalisedPets = pets.map((pet) => pet.toUpperCase());
 const petsStartingWithH = pets.filter((pet) => pet[0] === "h");
+
+function logPets(petsArr) {
+  petsArr.forEach((pet) => console.log(pet));
+}
 
 function countAndCapitalisePets(petsArr) {
   const petCount = {};
@@ -20,4 +25,4 @@ function countAndCapitalisePets(petsArr) {
 
 const countedPetsStartingWithH = countAndCapitalisePets(petsStartingWithH);
 
-console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 }
+console.log(countedPetsStartingWithH); // { 'HAMSTER': 3, 'HORSE': 1 } <- Final console log
